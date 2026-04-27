981c74842deae33fc52e1cb2930e2badc6a9185d
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2026-04-27 12:36:04 -0400

    Update index.js

--- client/index.js
+++ client/index.js
@@ -21,20 +21,10 @@ const ws = new WebSocket(
   "http://localhost:3000",
 );
 
-setInterval(() => {
-  if (ws.readyState === 1) {
-    ws.send(JSON.stringify({ type: "ping" }));
-  }
-}, 30000);
 
-ws.onclose = () => {
-  console.log("WebSocket deconnecte, rechargement de la page...");
-  setTimeout(() => location.reload(), 3000);
-};
 
-ws.onerror = (erreur) => {
-  console.error("Erreur WebSocket :", erreur);
-};
+
+
 
 var temp = 0;
 var lum = 0;
